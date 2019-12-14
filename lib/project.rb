fa6b2d7da277e2da993b1b843ff662df2f0e7dfe
Ashley Twomey <aprier091012@gmail.com>
2019-12-14 17:49:58 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,13 +1,14 @@
-class Project
-  attr_accessor :title, :backers
-  
-  def initialize(title)
-    @title = title
-    @backers = []
+class Backer
+
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
   end
 
-  def add_backer(backer)
-    @backers << backer
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
   end
 end
